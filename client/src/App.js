359da59402b38b99b4fe583d73a1a0a37d927beb
Spencer_Vail <vail.spencer@gmail.com>
2020-05-21 21:23:18 -0700

    initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateMovie from './Movies/UpdateMovie.js'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -30,6 +31,10 @@ const App = () => {
 
       <Route exact path="/">
         <MovieList movies={movieList} />
+      </Route>
+
+      <Route path='/update-movie/:id'>
+        <UpdateMovie />
       </Route>
 
       <Route path="/movies/:id">
